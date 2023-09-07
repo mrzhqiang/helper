6007d279d180d6af43c6434d24d5fcd107d162ff
mrzhyu <1058622030@qq.com>
2023-09-07 22:04:57 +0800

    修复 RSA 错误

--- helper/src/main/java/com/github/mrzhqiang/helper/RSADecrypts.java
+++ helper/src/main/java/com/github/mrzhqiang/helper/RSADecrypts.java
@@ -1,15 +1,11 @@
-package com.github.mrzhqiang.rowing.util;
+package com.github.mrzhqiang.helper;
 
-import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.crypto.Cipher;
 import java.nio.charset.StandardCharsets;
-import java.security.KeyFactory;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.PrivateKey;
-import java.security.PublicKey;
+import java.security.*;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
@@ -17,16 +13,16 @@ import java.util.Base64;
 /**
  * RSA 解密工具。
  */
-@Slf4j
 public final class RSADecrypts {
     private RSADecrypts() {
         // no instances.
     }
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(RSADecrypts.class);
+
     public static final int KEY_SIZE = 2048;
 
-    @SneakyThrows
-    public static KeyPair generateKeyPair() {
+    public static KeyPair generateKeyPair() throws Exception {
         KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
         generator.initialize(KEY_SIZE);
         return generator.generateKeyPair();
@@ -44,24 +40,21 @@ public final class RSADecrypts {
         return Base64.getEncoder().encodeToString(encoded);
     }
 
-    @SneakyThrows
-    public static PublicKey parsePublicKey(String publicText) {
+    public static PublicKey parsePublicKey(String publicText) throws Exception {
         byte[] decode = Base64.getDecoder().decode(publicText);
         X509EncodedKeySpec keySpec = new X509EncodedKeySpec(decode);
         KeyFactory factory = KeyFactory.getInstance("RSA");
         return factory.generatePublic(keySpec);
     }
 
-    @SneakyThrows
-    public static PrivateKey parsePrivateKey(String privateText) {
+    public static PrivateKey parsePrivateKey(String privateText) throws Exception {
         byte[] decode = Base64.getDecoder().decode(privateText);
         PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(decode);
         KeyFactory factory = KeyFactory.getInstance("RSA");
         return factory.generatePrivate(keySpec);
     }
 
-    @SneakyThrows
-    public static String encrypt(String plainText, PublicKey publicKey) {
+    public static String encrypt(String plainText, PublicKey publicKey) throws Exception {
         Cipher encryptCipher = Cipher.getInstance("RSA");
         encryptCipher.init(Cipher.ENCRYPT_MODE, publicKey);
         byte[] cipherText = encryptCipher.doFinal(plainText.getBytes(StandardCharsets.UTF_8));
@@ -75,7 +68,7 @@ public final class RSADecrypts {
             decryptCipher.init(Cipher.DECRYPT_MODE, privateKey);
             return new String(decryptCipher.doFinal(bytes), StandardCharsets.UTF_8);
         } catch (Exception e) {
-            log.error("RSA 解密失败！将直接返回文本：" + cipherText, e);
+            LOGGER.error("RSA 解密失败！将直接返回文本：" + cipherText, e);
             return cipherText;
         }
     }
