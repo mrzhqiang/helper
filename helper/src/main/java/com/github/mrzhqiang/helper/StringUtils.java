269da1b6afc69986de2c080b4b9664c8d9a5930e
zhangqiang <zhangqiang@phiz.live>
2024-07-16 09:18:24 +0800

    新增：字符串工具类。

---
+++ helper/src/main/java/com/github/mrzhqiang/helper/StringUtils.java
@@ -0,0 +1,20 @@
+package com.github.mrzhqiang.helper;
+
+import jdk.internal.joptsimple.internal.Strings;
+
+/**
+ * 字符串工具。
+ * <p>
+ * 模仿 Spring Framework 生成，后续不需要则删除。
+ */
+public final class StringUtils {
+
+    private StringUtils() {
+        // no instances.
+    }
+
+    public static boolean hasText(String str) {
+        return !Strings.isNullOrEmpty(str);
+    }
+
+}
